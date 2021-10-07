b28876838e2c7a95d31ca34889e5d9f70937c174
fletchernwilmott <fwilmottct@gmail.com>
2021-10-07 16:13:06 -0400

    started on product class

--- Backend/src/main/java/com/cogent/amazingbuy/model/Product.java
+++ Backend/src/main/java/com/cogent/amazingbuy/model/Product.java
@@ -1,10 +1,18 @@
 package com.cogent.amazingbuy.model;
 
-import org.springframework.data.annotation.Id;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
+@Entity
+@Table(name = "product")
 public class Product {
 	
 	@Id
-	private int id;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+	private Long id;
+	
+	
 
 }
