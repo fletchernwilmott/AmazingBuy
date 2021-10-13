cb34b7af1c01719c0fea3ffa00f8f8f092e53300
Mathboy64 <josue.blanco49102@outlook.com>
2021-10-13 17:32:32 -0500

    Reset old changes

---
+++ amazingbuy-frontend/src/app/login/login.component.ts
@@ -0,0 +1,36 @@
+import { ifStmt } from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { TestAccount } from '../test-account';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  testaccountb = new TestAccount('email@gmail.com', 'MyPassword');
+  myform:any={}
+  submitted=false;
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log('hello');
+  }
+  
+  
+  onSubmit() {
+    this.submitted = true;
+    console.log('Email: ' + this.testaccountb.email);
+    console.log('Password: ' + this.testaccountb.password);
+
+    this.CheckAccount(this.testaccountb.email, this.testaccountb.password);
+    AssignSignedAccount();
+  }
+
+
+}
+
+
